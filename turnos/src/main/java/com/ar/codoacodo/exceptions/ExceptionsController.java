6ebf80760b0d30e5fd26a1d8e0e941cedcd45a69
Sabrina <marchiori.sabrina@hotmail.com>
2023-07-11 01:53:09 -0300

    clase 27/06

---
+++ turnos/src/main/java/com/ar/codoacodo/exceptions/ExceptionsController.java
@@ -0,0 +1,15 @@
+package com.ar.codoacodo.exceptions;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+@ControllerAdvice
+public class ExceptionsController {
+	
+	@ExceptionHandler(MethodArgumentNotValidException.class)
+	public ResponseEntity<?> fallaValidacion(MethodArgumentNotValidException e) {
+		return ResponseEntity.badRequest().body(e.getMessage());
+	}
+}
